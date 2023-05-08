0014431aa0fe3438ac890ef05d89df770e79a24d
apriori85 <victor-timoshin@hotmail.com>
2023-05-08 10:14:52 +0300

    Added render.hpp

---
+++ lib/include/sway/render.hpp
@@ -0,0 +1,14 @@
+#ifndef SWAY_RENDER_HPP
+#define SWAY_RENDER_HPP
+
+#include <sway/render/effect.hpp>
+#include <sway/render/geometry.hpp>
+#include <sway/render/image.hpp>
+#include <sway/render/procedurals/guides/axis.hpp>
+#include <sway/render/procedurals/prims/plane.hpp>
+#include <sway/render/renderqueue.hpp>
+#include <sway/render/rendersubqueue.hpp>
+#include <sway/render/rendersubqueuegroups.hpp>
+#include <sway/render/rendersubsystem.hpp>
+
+#endif  // SWAY_RENDER_HPP
