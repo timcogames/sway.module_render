3b11f32be8649fa1b007ae5e079e3e8cc54287a5
apriori85 <victor-timoshin@hotmail.com>
2024-01-10 18:49:25 +0300

    Updated submodule

--- lib/cxx/include/sway/render.hpp
+++ lib/cxx/include/sway/render.hpp
@@ -3,6 +3,7 @@
 
 #include <sway/render/effect.hpp>
 #include <sway/render/geometry.hpp>
+#include <sway/render/geometrycreateinfo.hpp>
 #include <sway/render/geometryindexedvertexdata.hpp>
 #include <sway/render/geometryvertexattrib.hpp>
 #include <sway/render/geometryvertexattribset.hpp>
@@ -11,9 +12,15 @@
 #include <sway/render/material.hpp>
 #include <sway/render/materialdescriptor.hpp>
 #include <sway/render/materialmanager.hpp>
+#include <sway/render/pipeline/rendercommand.hpp>
+#include <sway/render/pipeline/rendercommandhandler.hpp>
 #include <sway/render/postprocessing.hpp>
 #include <sway/render/procedurals/guides/axis.hpp>
 #include <sway/render/procedurals/prims/plane.hpp>
+#include <sway/render/procedurals/prims/plane_debug.hpp>
+#include <sway/render/procedurals/prims/plane_list.hpp>
+#include <sway/render/procedurals/prims/plane_strip.hpp>
+#include <sway/render/procedurals/shape.hpp>
 #include <sway/render/rendercomponent.hpp>
 #include <sway/render/renderpass.hpp>
 #include <sway/render/renderqueue.hpp>
@@ -22,7 +29,9 @@
 #include <sway/render/rendersubqueuegroups.hpp>
 #include <sway/render/rendersubsystem.hpp>
 #include <sway/render/rendertarget.hpp>
+#include <sway/render/silhouettepostprocessing.hpp>
 #include <sway/render/sprite.hpp>
+#include <sway/render/sprite_debug.hpp>
 #include <sway/render/spriteanimation.hpp>
 #include <sway/render/spriteanimationclip.hpp>
 #include <sway/render/spriteanimationmodes.hpp>
