9a5180e16f12852f17a6cff0bdd09011ad934ead
apriori85 <victor-timoshin@hotmail.com>
2024-09-14 19:11:51 +0300

    Setted clear color

--- lib/cxx/src/rendertarget.cpp
+++ lib/cxx/src/rendertarget.cpp
@@ -5,7 +5,10 @@ NAMESPACE_BEGIN(render)
 
 void RenderTarget::setScissorViewport(gapi::ViewportPtr_t viewport) { viewport_ = viewport; }
 
-void RenderTarget::activate() { viewport_->clear(); }
+void RenderTarget::activate() {
+  viewport_->setClearColor(math::col4f_t(200.0F, 200.0F, 200.0F, 1.0F));
+  viewport_->clear();
+}
 
 void RenderTarget::deactivate() {}
 
