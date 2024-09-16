5af180d6c34a5aeb05a20f02fd638c8f92e688ed
apriori85 <victor-timoshin@hotmail.com>
2024-09-16 09:43:07 +0300

    Updated submodule

--- lib/cxx/src/rendertarget.cpp
+++ lib/cxx/src/rendertarget.cpp
@@ -6,8 +6,9 @@ NAMESPACE_BEGIN(render)
 void RenderTarget::setScissorViewport(gapi::ViewportPtr_t viewport) { viewport_ = viewport; }
 
 void RenderTarget::activate() {
-  viewport_->setClearColor(math::col4f_t(100.0F, 100.0F, 100.0F, 1.0F));
-  viewport_->clear(gapi::ClearFlag::COLOR | gapi::ClearFlag::DEPTH | gapi::ClearFlag::STENCIL);
+  viewport_->setClearColor(math::col4f_t(50.0F, 50.0F, 50.0F, 1.0F));
+  // viewport_->clear(gapi::ClearFlag::COLOR | gapi::ClearFlag::DEPTH | gapi::ClearFlag::STENCIL);
+  viewport_->clear(gapi::ClearFlag::NONE);
 }
 
 void RenderTarget::deactivate() {}
